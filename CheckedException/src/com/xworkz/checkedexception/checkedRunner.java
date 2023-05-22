0a13a91c2adf6095eeab956ef4545e2f38dbe8c2
unknown <you@example.com>
2023-05-22 19:05:57 +0530

    ExceptionDelegate codes

--- CheckedException/src/com/xworkz/checkedexception/checkedRunner.java
+++ CheckedException/src/com/xworkz/checkedexception/checkedRunner.java
@@ -8,21 +8,34 @@ public class checkedRunner {
 		
 		try
 		{
-			//FileInputStream refrence = new FileInputStream("C:\\Users\\Lenovo\\Desktop\\codes\\Byteloop.java");
-			FileInputStream refrence1 = new FileInputStream("C:\\Users\\Lenovo\\Desktop\\codes\\Byteloop");//campilarmayfindmaynotfind
-		
+//			FileInputStream refrence = new FileInputStream("C:\\Users\\Lenovo\\Desktop\\codes\\Byteloop.java");
+//			FileInputStream refrence1 = new FileInputStream("C:\\Users\\Lenovo\\Desktop\\codes\\Byteloop");//campilarmayfindmaynotfind
+	   
+		//	ClassNotFoundException
+			Class.forName ("fffygffg");
+	
 			System.out.println("hello");
 			
 			
 			System.out.println("welcome");
 		}
 		
-		catch
-		(Exception ref) {
-			System.out.println("not found file");
-			
-			System.out.println("runnering");
+//		catch
+//		(Exception ref) {
+//			
+//		ref.printStackTrace();
+//			System.out.println("not found file");
+//			
+//			System.out.println("runnering");
+//			
+//		}
+		
+			catch
+			(ClassNotFoundException cnf) {
+				cnf.printStackTrace();
+				System.out.println("ClassNotFoundException Handled");
 		}
+		
 	}
 
 }
